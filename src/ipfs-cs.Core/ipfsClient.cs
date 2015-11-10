5bc3d7b78fc7e5d297c93e2f652599d92d270e4f
CompulsiveCoder <compulsivecoder@gmail.com>
2015-11-10 12:18:13 +1100

    Initial commit

---
+++ src/ipfs-cs.Core/ipfsClient.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace ipfsecho.Core
+{
+	public class ipfsClient
+	{
+		public ipfsClient ()
+		{
+		}
+
+		public string AddFile(string filePath)
+		{
+			var starter = new ProcessStarter ();
+
+			starter.Start (
+				String.Format ("ipfs add {0}", Path.GetFileName (filePath))
+			);
+
+			var hash = ExtractHashAfterAddFile(starter.Output);
+
+			return hash;
+		}
+
+		public string ExtractHashAfterAddFile(string output)
+		{
+			var firstStep = output.Trim ().Substring (output.IndexOf (" ")).TrimStart();
+
+			var hash = firstStep.Substring (0, firstStep.IndexOf (" "));
+
+			return hash;
+		}
+	}
+}
+
