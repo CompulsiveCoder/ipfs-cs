c641efe7eae673daa1c0faea81a4bc07326d11b3
CompulsiveCoder <compulsivecoder@gmail.com>
2016-02-28 20:21:40 +1100

    Added run-from-github script

--- init.sh
+++ init.sh
@@ -1,3 +1,6 @@
+sudo apt-get update
+sudo apt-get install -y mono-base
+
 cd lib
 sh get-libs.sh
 cd ..
---
+++ run-from-github.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/CompulsiveCoder/ipfs-echo.git
+cd ipfs-echo
+sh init-build-run.sh
