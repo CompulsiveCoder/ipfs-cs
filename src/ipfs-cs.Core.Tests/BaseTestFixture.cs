2b56673ad24e49fb173ad6d2a9bf1a02b884fec5
CompulsiveCoder <compulsivecoder@gmail.com>
2016-02-28 18:55:11 +1100

    Added missing files

---
+++ src/ipfs-cs.Core.Tests/BaseTestFixture.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using NUnit.Framework;
+using System.IO;
+
+namespace ipfs.Core.Tests
+{
+	public class BaseTestFixture
+	{
+		public string OriginalDirectory;
+
+		public bool DeleteTemporaryDirectory = true;
+
+		public BaseTestFixture ()
+		{
+		}
+
+		[SetUp]
+		public void SetUp()
+		{
+			OriginalDirectory = Environment.CurrentDirectory;
+
+			var tmpDir = new TemporaryDirectoryCreator ().Create ();
+
+			Directory.SetCurrentDirectory (tmpDir);
+
+			Console.WriteLine ("Current directory:");
+			Console.WriteLine (tmpDir);
+			Console.WriteLine ();
+		}
+
+		[TearDown]
+		public void TearDown()
+		{
+			var tmpDir = Environment.CurrentDirectory;
+
+			Directory.SetCurrentDirectory (OriginalDirectory);
+
+			if (DeleteTemporaryDirectory) {
+				Directory.Delete (tmpDir, true);
+			}
+		}
+	}
+}
+
