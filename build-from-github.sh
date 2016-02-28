7e724a7ea6e033f98e44dbd5586faa282eb03073
CompulsiveCoder <compulsivecoder@gmail.com>
2016-02-28 20:41:52 +1100

    Updated build scripts

---
+++ build-from-github.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/CompulsiveCoder/ipfs-cs.git
+cd ipfs-echo
+sh init-build.sh
--- run-from-github.sh
+++
@@ -1,3 +0,0 @@
-git clone https://github.com/CompulsiveCoder/ipfs-echo.git
-cd ipfs-echo
-sh init-build-run.sh
