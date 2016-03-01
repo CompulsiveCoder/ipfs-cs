82563298c8573b4bcd1a11d7eac61496e84139e8
CompulsiveCoder <compulsivecoder@gmail.com>
2016-03-02 09:49:08 +1100

    Added nunit.runners

--- lib/get-libs.sh
+++ lib/get-libs.sh
@@ -2,4 +2,5 @@ wget http://nuget.org/nuget.exe
 
 mozroots --import --sync
 
-mono nuget.exe install nunit
+mono nuget.exe install nunit -version 2.6.4
+mono nuget.exe install nunit.runners -version 2.6.4
