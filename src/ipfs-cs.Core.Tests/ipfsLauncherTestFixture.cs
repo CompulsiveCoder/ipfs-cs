7856fb570e376f2807ec41a2f0d3fc6a8ddca74c
CompulsiveCoder <compulsivecoder@gmail.com>
2016-03-02 11:35:44 +1100

    Unfinished

---
+++ src/ipfs-cs.Core.Tests/ipfsLauncherTestFixture.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+using System.Threading;
+
+namespace ipfs.Core.Tests
+{
+	[TestFixture]
+	public class ipfsLauncherTestFixture
+	{
+		[Test]
+		public void Test_Start()
+		{
+			using (var launcher = new ipfsLauncher ()) {
+				launcher.Start ();
+				Thread.Sleep (10000);
+				launcher.Close ();
+			}
+		}
+	}
+}
+
---
+++ src/ipfs-cs.Core/ipfsLauncher.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.IO;
+using System.Threading;
+
+namespace ipfs.Core
+{
+	public class ipfsLauncher : Component
+	{
+		Thread IpfsThread;
+		Process IpfsProcess;
+
+		public ipfsLauncher ()
+		{
+		}
+
+		public void Start()
+		{
+			IpfsThread = new System.Threading.Thread(StartProcess);
+
+			IpfsThread.Start ();
+		}
+
+		public void StartProcess()
+		{
+			var ipfsScriptPath = Path.GetFullPath ("../../run-ipfs-for-tests.sh");
+
+			var ipfsProcess = new Process ();
+			IpfsProcess = ipfsProcess; // TODO: Clean up code
+			ipfsProcess.StartInfo = new ProcessStartInfo ("sh", ipfsScriptPath + " >> ipfsdaemon.log");
+			//ipfsProcess.StartInfo.WorkingDirectory = dataPath;
+			//ipfsProcess.StartInfo.CreateNoWindow = true;
+
+			ipfsProcess.StartInfo.UseShellExecute = true;
+			/*ipfsProcess.StartInfo.RedirectStandardInput = true;
+			ipfsProcess.StartInfo.RedirectStandardOutput = true;
+			ipfsProcess.StartInfo.RedirectStandardError = true;*/
+			//ipfsProcess.
+			ipfsProcess.Start ();
+
+			//Thread.Sleep (10000);
+			//ipfsProcess.Kill ();
+		}
+
+		public void Close()
+		{
+			if (IpfsThread != null) {
+			//	IpfsProcess.Kill ();
+				IpfsThread.Abort ();
+			}
+		}
+
+		protected override void Dispose (bool release_all)
+		{
+			Close ();
+			if (IpfsThread != null) {
+				IpfsProcess.Dispose ();
+				IpfsThread = null;
+			}
+
+			base.Dispose (release_all);
+		}
+	}
+}
+
