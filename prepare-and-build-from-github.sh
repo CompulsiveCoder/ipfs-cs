be805a5a7c362176d26647e0f85eea15b4394103
CompulsiveCoder <compulsivecoder@gmail.com>
2016-03-11 11:06:50 +1100

    Changed 'sh' to '/bin/bash' so it works in docker

--- prepare-and-build-from-github.sh
+++ prepare-and-build-from-github.sh
@@ -1,3 +1,3 @@
-curl https://raw.githubusercontent.com/CompulsiveCoder/ipfs-cs/master/prepare.sh | sh
-curl https://raw.githubusercontent.com/CompulsiveCoder/ipfs-cs/master/build-from-git.sh | sh
+curl https://raw.githubusercontent.com/CompulsiveCoder/ipfs-cs/master/prepare.sh | /bin/bash
+curl https://raw.githubusercontent.com/CompulsiveCoder/ipfs-cs/master/build-from-git.sh | /bin/bash
 
