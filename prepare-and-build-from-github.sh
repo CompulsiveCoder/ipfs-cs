35441fcabeaec1b710503d0a34b637ba5f048d6c
CompulsiveCoder <compulsivecoder@gmail.com>
2016-03-15 10:19:43 +1100

    Fixed scripts

--- prepare-and-build-from-github.sh
+++ prepare-and-build-from-github.sh
@@ -1,8 +1,14 @@
 BRANCH=$1
 
 if [ -z "$BRANCH" ]; then
+    BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+fi
+
+if [ -z "$BRANCH" ]; then
     BRANCH="master"
 fi
+
+echo "Branch: $BRANCH"
 
 curl https://raw.githubusercontent.com/CompulsiveCoder/ipfs-cs/$BRANCH/prepare.sh | sh
 curl https://raw.githubusercontent.com/CompulsiveCoder/ipfs-cs/$BRANCH/build-from-github.sh | sh -s $BRANCH
--- prepare-and-test-from-github.sh
+++ prepare-and-test-from-github.sh
@@ -1,8 +1,14 @@
 BRANCH=$1
 
 if [ -z "$BRANCH" ]; then
+    BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+fi
+
+if [ -z "$BRANCH" ]; then
     BRANCH="master"
 fi
+
+echo "Branch: $BRANCH"
 
 curl https://raw.githubusercontent.com/CompulsiveCoder/ipfs-cs/$BRANCH/prepare.sh | sh
 curl https://raw.githubusercontent.com/CompulsiveCoder/ipfs-cs/$BRANCH/test-from-github.sh | sh -s $BRANCH
