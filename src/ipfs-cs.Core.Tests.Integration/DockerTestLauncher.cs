616d61e8e070370aa35f8a8674cf0512324c4330
CompulsiveCoder <compulsivecoder@gmail.com>
2016-03-16 21:19:29 +1100

    Trying to fix docker issues

--- src/ipfs-cs.Core.Tests.Integration/DockerTestLauncher.cs
+++ src/ipfs-cs.Core.Tests.Integration/DockerTestLauncher.cs
@@ -32,7 +32,7 @@ namespace ipfs.Core.Tests.Integration
 			//var command = String.Format("cp /ipfs-cs /ipfs-cs-staging -r && cd /ipfs-cs-staging && rm bin/* -r && sh build.sh {0} && cd bin/{0} && mono LaunchIntegrationTest.exe /assembly:\"{1}\" /type:\"{2}\"", buildMode, assemblyName, fixtureType.FullName);
 			var command = String.Format("cd /ipfs-cs && sh start-integration-test.sh {0} {1} {2}", buildMode, assemblyName, fixtureType.FullName);
 
-			command = String.Format ("docker run -i -v {0}:/ipfs-cs {1} /bin/bash -c '{2}'", projectPath, ImageName, command);
+			command = String.Format ("docker run -i -v /var/run/docker.sock:/var/run/docker.sock -v {0}:/ipfs-cs {1} /bin/bash -c '{2}'", projectPath, ImageName, command);
 			starter.WriteToConsole = true;
 			starter.Start (
 				command
