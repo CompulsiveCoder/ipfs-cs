1e7fbf90b62f0eff6c6778a46ff92a693f245487
CompulsiveCoder <compulsivecoder@gmail.com>
2016-03-16 16:53:04 +1100

    Tweaked test via docker script

--- test-from-github-via-docker.sh
+++ test-from-github-via-docker.sh
@@ -10,4 +10,4 @@ fi
 
 echo "Branch: $BRANCH"
 
-docker run -it compulsivecoder/ubuntu-mono /bin/bash -c "curl https://raw.githubusercontent.com/CompulsiveCoder/ipfs-cs/$BRANCH/test-from-github.sh | sh -s $BRANCH"
+docker run -i compulsivecoder/ubuntu-mono /bin/bash -c "curl https://raw.githubusercontent.com/CompulsiveCoder/ipfs-cs/$BRANCH/test-from-github.sh | sh -s $BRANCH"
