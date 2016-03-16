25757cedd0afa531d16c289d466c2252de4611e9
CompulsiveCoder <compulsivecoder@gmail.com>
2016-03-16 16:59:43 +1100

    Still fixing issues with running tests on travis ci

--- start-integration-test.sh
+++ start-integration-test.sh
@@ -1,6 +1,7 @@
+cd ..
 cp /ipfs-cs /ipfs-cs-staging -r
 cd /ipfs-cs-staging
 rm bin/* -r
 sh build.sh $1
 cd bin/$1
-ipfs daemon & sleep 5 && mono LaunchIntegrationTest.exe /assembly:"$2" /type:"$3"
+ipfs daemon & sleep 7 && mono LaunchIntegrationTest.exe /assembly:"$2" /type:"$3"
