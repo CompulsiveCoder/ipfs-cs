0a660999d6ead2ecf20b391279570a8c8515affd
CompulsiveCoder <compulsivecoder@gmail.com>
2016-03-17 19:44:47 +1100

    Finally fixed integration testing on travis ci by removing the -i flag from the docker call

--- src/ipfs-cs.Core.Tests.Integration/DockerTestLauncher.cs
+++ src/ipfs-cs.Core.Tests.Integration/DockerTestLauncher.cs
@@ -34,7 +34,7 @@ namespace ipfs.Core.Tests.Integration
 			// TODO: Clean ups
 			//var command = String.Format("cp /ipfs-cs /ipfs-cs-staging -r && cd /ipfs-cs-staging && rm bin/* -r && sh build.sh {0} && cd bin/{0} && mono LaunchIntegrationTest.exe /assembly:\"{1}\" /type:\"{2}\"", buildMode, assemblyName, fixtureType.FullName);
 
-			var command = String.Format("cd /ipfs-cs && sh start-integration-test.sh {0} {1} {2}", buildMode, assemblyName, fixtureType.FullName);
+			var command = String.Format("cd /ipfs-cs && pwd && ls && sh start-integration-test.sh {0} {1} {2}", buildMode, assemblyName, fixtureType.FullName);
 
 			command = String.Format ("docker run -v /var/run/docker.sock:/var/run/docker.sock -v {0}:/ipfs-cs {1} /bin/bash -c '{2}'", projectPath, ImageName, command);
 			starter.WriteToConsole = true;
